1e05a3a06454f4762a3d0091fd8492930cc121ff
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-10-18 00:55:41 +0800

    add reverse slice

--- intslice.go
+++ intslice.go
@@ -1,4 +1,5 @@
 package leetcomm
+
 func IntSliceEqual(a, b []int) bool {
 	if len(a) != len(b) {
 		return false
@@ -9,6 +10,19 @@ func IntSliceEqual(a, b []int) bool {
 		}
 	}
 	return true
+}
+
+func IntSliceEqualIgnoreOrder(a, b []int) bool {
+	xor, suma, sumb := 0, 0, 0
+	for _, val := range a {
+		xor ^= val
+		suma += val * val
+	}
+	for _, val := range b {
+		xor ^= val
+		sumb += val * val
+	}
+	return xor == 0 && suma == sumb
 }
 
 func Int2DSliceEqual(a, b [][]int) bool {
@@ -32,4 +46,11 @@ func DupSlice(a []int) []int {
 	ret := make([]int, len(a))
 	copy(ret, a)
 	return ret
+}
+
+func ReverseSlice(a []int) {
+	l := len(a)
+	for i := 0; i < l/2; i++ {
+		a[i], a[l-1-i] = a[l-1-i], a[i]
+	}
 }
